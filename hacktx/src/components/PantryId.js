9b05ceaac4d96851909eaa711812683982598b7f
Ted Kahl <tedkahl@gmail.com>
2020-10-25 00:09:40 -0700

    working, slightly buggy pantry page

--- hacktx/src/components/PantryId.js
+++ hacktx/src/components/PantryId.js
@@ -1,26 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { db } from "../firebase";
 
-const getPantry = async (id) => {
-  let doc = await db.collection("pantries").doc(id).get();
-  console.log(doc);
-  return doc.data();
-};
-
-async function PantryId(props) {
+const PantryId = (props) => {
+  const [loaded, setLoaded] = useState(0);
   const [pantry, setPantry] = useState({});
   const [item, setItem] = useState("Sliced Bread");
   const [itemno, setItemNo] = useState(0);
 
   useEffect(() => {
-    getPantry(props.match.params.id)
-      .then((doc) => {
-        setPantry(doc);
-        console.log(doc);
-      })
-      .catch((err) => {
-        console.log("error:", err);
-      });
+    async function fetchData() {
+      console.log("at least were here");
+      let doc = await db
+        .collection("pantries")
+        .doc(props.match.params.id)
+        .get();
+      console.log(doc.data());
+      setPantry(doc.data());
+      setLoaded(true);
+    }
+    fetchData();
   }, []);
 
   const takeItem = (key) => {
@@ -36,7 +34,7 @@ async function PantryId(props) {
   };
 
   const onChangeItemNo = (event) => {
-    setItemNo(event.target.value);
+    setItemNo(parseInt(event.target.value));
     console.log(event.target.value);
   };
 
@@ -50,47 +48,50 @@ async function PantryId(props) {
     await db.collection("pantries").doc(props.match.params.id).set(pantry);
   };
   console.log("pantry:", pantry);
-  return (
-    <div>
-      <h1>{pantry.name}</h1>
-      <h3>location</h3>
+  if (loaded) {
+    return (
       <div>
-        <p>Inventory:</p>
-        {Object.keys(pantry.inventory).map((key) => {
-          return (
-            <div>
-              <p>{key + ":" + pantry.inventory[key]}</p>
-              <button
-                onClick={() => {
-                  takeItem(key);
-                }}
-              >
-                Take item
-              </button>
-            </div>
-          );
-        })}
+        <h1>{pantry.name}</h1>
+        <h3>Location:{pantry.lat + "," + pantry.lng}</h3>
+        <div>
+          <p>Inventory:</p>
+          {Object.keys(pantry.inventory).map((key) => {
+            return (
+              <div label={key}>
+                <p>{key + ":" + pantry.inventory[key]}</p>
+                <button
+                  onClick={() => {
+                    takeItem(key);
+                  }}
+                >
+                  Take item
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <label htmlFor="Add item:">Item:</label>
+          <input
+            type="text"
+            name="invitem"
+            placeholder={item}
+            onChange={onChangeItem}
+          />
+          <label htmlFor="itemno">Amount:</label>
+          <input
+            type="text"
+            name="itemNo"
+            placeholder={itemno}
+            onChange={onChangeItemNo}
+          />
+        </div>
+        <button onClick={() => addItem()}>Add item</button>
+        <button onClick={save}>Save!</button>
       </div>
-      <div>
-        <label htmlFor="item">Item:</label>
-        <input
-          type="text"
-          name="invitem"
-          placeholder={item}
-          onChange={onChangeItem}
-        />
-        <label htmlFor="itemno">Amount:</label>
-        <input
-          type="text"
-          name="itemNo"
-          placeholder={itemno}
-          onChange={onChangeItemNo}
-        />
-      </div>
-      <button onClick={() => addItem()}>Add item</button>
-      <button onClick={save}>Save!</button>
-    </div>
-  );
-}
+    );
+  } else return <div>loading</div>;
+};
 
 export default PantryId;
+/**/
